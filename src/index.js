488acfde89256b43bcbe2c95840abbc1207fb6b2
totalintelli <blueintelli@gmail.com>
2020-11-30 21:34:53 +0900

    delete src and public

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// src/index.js
-import calc from "./utils/utility";
-
-let x = 6;
-let y = 5;
-let str = `<h2>${x} + ${y} = ${calc.add(x, y)}</h2>`;
-
-document.getElementById("app").inerHTML = str;
